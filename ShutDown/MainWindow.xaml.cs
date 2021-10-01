06194c81afebfe4690f2c12efe9c1d55be356109
pcliu <liugodlol@gmail.com>
2021-10-01 23:17:26 +0800

    cmd command function done

--- ShutDown/MainWindow.xaml.cs
+++ ShutDown/MainWindow.xaml.cs
@@ -30,12 +30,19 @@ namespace ShutDown
     void OnClick(object sender, RoutedEventArgs e)
     {
       DateTime? selectedDate = dpicker.SelectedDate ?? DateTime.Now;
-      string formatted = selectedDate.Value.ToString("yyyy-MM-dd", System.Globalization.CultureInfo.InvariantCulture);
-
+      DateTime? selectedTime = tpciker.Value ?? DateTime.Now;
+      string date = selectedDate.Value.ToString("yyyy-MM-dd", System.Globalization.CultureInfo.InvariantCulture);
+      string time = selectedTime.Value.ToString("HH:mm:ss", System.Globalization.CultureInfo.InvariantCulture);
+      StringBuilder sb = new StringBuilder();
+      sb.Append(date);
+      sb.Append(" ");
+      sb.Append(time);
+      string datetime = sb.ToString();
+      shutting.CalDiff(datetime);
     }
     void OnClick2(object sender, RoutedEventArgs e)
     {
-
+      shutting.CancelCommand();
     }
   }
 }
--- ShutDown/shutting.cs
+++ ShutDown/shutting.cs
@@ -1,16 +1,83 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace ShutDown
 {
   internal class shutting
   {
-    public void gettime()
-    {
 
+    public static void CalDiff(string datetime)
+    {
+      DateTime dt = DateTime.Parse(datetime);
+      DateTime now = DateTime.Now;
+      if (dt.CompareTo(now) < 0)
+      {
+        _ = MessageBox.Show("Input time must greater than now", "Input error", MessageBoxButton.OK, MessageBoxImage.Error);
+
+      }
+      else
+      {
+        TimeSpan dif = dt - now;
+        double diff = Math.Round(dif.TotalSeconds);
+        ShutDownCommand(diff);
+      }
+    }
+    public static void ShutDownCommand(double diff)
+    {
+      try
+      {
+        string cmd = $"shutdown -s -t {diff}";
+        Process process = new Process();
+        process.StartInfo.FileName = "cmd.exe";
+        process.StartInfo.UseShellExecute = false;
+        process.StartInfo.RedirectStandardOutput = true;
+        process.StartInfo.RedirectStandardInput = true;
+        process.StartInfo.RedirectStandardError = true;
+        process.StartInfo.CreateNoWindow = true;
+        process.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+        process.Start();
+        process.StandardInput.WriteLine("cd\\");
+        process.StandardInput.WriteLine(cmd);
+        process.StandardOutput.ReadToEnd();
+        process.WaitForExit();
+        process.Close();
+        _ = MessageBox.Show("cancel");
+      }
+      catch (Exception ex)
+      {
+        _ = MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+      }
+      
+    }
+    public static void CancelCommand()
+    {
+      try
+      {
+        Process process = new Process();
+        process.StartInfo.FileName = "cmd.exe";
+        process.StartInfo.UseShellExecute = false;
+        process.StartInfo.RedirectStandardOutput = true;
+        process.StartInfo.RedirectStandardInput = true;
+        process.StartInfo.RedirectStandardError = true;
+        process.StartInfo.CreateNoWindow = true;
+        process.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+        process.Start();
+        process.StandardInput.WriteLine("cd\\");
+        process.StandardInput.WriteLine("shutdown -A");
+        process.StandardOutput.ReadToEnd();
+        process.WaitForExit();
+        process.Close();
+        _ = MessageBox.Show("cancel");
+      }
+      catch (Exception ex)
+      {
+        _ = MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+      }
     }
   }
 }
